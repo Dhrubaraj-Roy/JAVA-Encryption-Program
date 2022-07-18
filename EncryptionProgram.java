1c6793ef60ada6de7a0ec4cc872b659d7b7c3e5f
Djrubaraj-Roy <dhrubarajroy1233@gmail.com>
2022-07-18 12:37:59 +0530

    working on askquestion() method

--- EncryptionProgram.java
+++ EncryptionProgram.java
@@ -1,15 +1,72 @@
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
+
+import javax.xml.transform.Source;
 
 public class EncryptionProgram {
     private Scanner scanner;
+    private Random random;
+    private ArrayList<Character> list;
+    private ArrayList<Character> shuffledList;
+    private char characters;
+    private String line;
+    private char[] letters;
+    private char[] secretLetters;
+
+
+    
+
+
     EncryptionProgram(){
+
+        scanner = new Scanner(System.in);
+        random = new Random();
+        list = new ArrayList();
+        shuffledList = new ArrayList();
+        characters = ' ';
+        //newkey();
+        askquestion();
+
 
     }
     private void askquestion(){
+        while(true){
+            System.out.println("**********************************");
+            System.out.println("What do you want to do? ");
+            System.out.println("(N)ewKey, (G)etKey, (E)ncrypt, (D)ecrypt, (Q)uit ");
+            char responce = Character.toUpperCase(scanner.nextLine().charAt(0));
+
+            switch(responce){
+                case 'N':
+                    newkey();
+                    break;
+                case 'G':
+                    getClass();
+                    break;
+                case 'E':
+                    encrypt();
+                    break;
+                case 'D':
+                    decrypt();
+                    break;
+                case 'Q':
+                    quit();
+                    break;
+                default:
+                    System.out.println("Please enter vaild key :( ");
+            
+            }
+        }
+
+
+        
+        
 
         
     }
     private void newkey() {
+        System.out.println("Its working !!");
             
     }
     private void getkey() {
---
+++ test.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class test{
+    public static void main(String[] args) {
+        // Random random = new Random();
+        // int x = random.nextInt(6)+1;
+        // System.out.println(x);
+
+
+
+        // ArrayList<String> food = new ArrayList<String>();
+        // food.add("Dhruba");
+        // food.add("Tatha");
+        // food.add("Sayon");
+        // food.set(1, "element");
+        // food.remove(1);
+        // food.clear();
+        // // System.out.println(food.toString());
+        // for(int i=0; i<food.size();i++){
+        //     System.out.println(food.get(i));
+        //}
+    }
+    
+}
